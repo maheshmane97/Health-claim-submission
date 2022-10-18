4566f66163e692b0e0e6ab4dd3136bffb605cf4c
maheshmane97 <maheshkumarmane70@gmail.com>
2022-10-18 11:42:00 +0530

    Physician resolved

--- hcm/src/main/java/com/hnt/hcm/serviceImpl/MemberServiceImpl.java
+++ hcm/src/main/java/com/hnt/hcm/serviceImpl/MemberServiceImpl.java
@@ -17,66 +17,51 @@ import com.hnt.hcm.repository.PhysicianRepository;
 import com.hnt.hcm.service.MemberService;
 
 import lombok.extern.slf4j.Slf4j;
+
 @Service
 @Slf4j
 public class MemberServiceImpl implements MemberService {
-	
+
 	@Autowired
 	MemberRepository memberRepository;
-	
+
 	@Autowired
 	PhysicianRepository physicianRepository;
-	
+
 	@Autowired
 	ClaimRepository claimRepo;
 
 	@Override
 	public Member addMember(Member member) {
-	List<Physician> physician=physicianRepository.findAll().stream().filter(e->e.getStatus().equalsIgnoreCase("false")).collect(Collectors.toList());
-		Member mem=null;
-		for(Physician ph: physician) {
-			while(ph.getStatus().equalsIgnoreCase("false")) {
-				//ph.setStatus("true");
-				member.setPhysician(ph);
-				break;
+		Member mem = null;
+
+		Iterator<Physician> itr = physicianRepository.findByStatus("false").iterator();
+		if (itr.hasNext()) {
+			Physician physi = itr.next();
+			member.setPhysician(physi);
+			physi.setStatus("true");
+			mem = memberRepository.save(member);
 		}
-		mem=memberRepository.save(member);
-		}
-		
-//		Iterator<Physician> itr=physicianList.iterator();
-//		while(itr.hasNext()) {
-//			Physician p=itr.next();
-//			member.setPhysician(p.getPhysicianId());
-//			memberRepository.save(member);
-//		}
-		
-		
-//		physicianList.forEach(e->{
-//			e.getStatus().equalsIgnoreCase("false");
-//			Member mem=memberRepository.save(member);
-//			e.setStatus("true");
-//		});
+
 		return mem;
 	}
-
 
 	@Override
 	public List<Member> getAllMembers() {
 		return memberRepository.findAll();
 	}
-	
+
 	@Override
 	public List<Member> getMember(String firstName, String lastName, String physicianName, Integer claimId) {
-		List<Member> list=memberRepository.findAll();
-		
-		List<Member> member=list.stream().filter(e->e.getFirstName().equalsIgnoreCase(firstName) || 
-				e.getLastName().equalsIgnoreCase(lastName) ||  
-				e.getPhysician().getPhysicianName().equalsIgnoreCase(physicianName) ||
-				e.getClaims().getClaimId() == claimId
-				).collect(Collectors.toList());
-		
+		List<Member> list = memberRepository.findAll();
+
+		List<Member> member = list.stream()
+				.filter(e -> e.getFirstName().equalsIgnoreCase(firstName) || e.getLastName().equalsIgnoreCase(lastName)
+						|| e.getPhysician().getPhysicianName().equalsIgnoreCase(physicianName)
+						|| e.getClaims().getClaimId() == claimId)
+				.collect(Collectors.toList());
+
 		return member;
 	}
-	
 
 }
